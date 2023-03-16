43c2ac551100c4f511f7940076d9c1eb1027ca39
guot54 <guot55@163.com>
2023-03-16 08:29:47 +0000

    更新 'zB/直播.js'

--- zB/直播.js
+++ zB/直播.js
@@ -3,7 +3,6 @@
 {"name": "巧计",     "url": "https://ghproxy.com/https://raw.githubusercontent.com/dxawi/1/main/tvlive.txt"},
 {"name": "星辰",     "url": "http://tvmvip.com/xclive.txt"},
 {"name": "俊于",     "url": "http://home.jundie.top:81/Cat/tv/live.txt"},
-{"name": "多多",     "url": "https://yydsys.top/duo/txt/v.txt"},
 {"name": "菜妮丝",     "url": "http://xn--ihqu10cn4c.xn--z7x900a.love:63/TV/tvzb.txt"},
 {"name": "乱世",     "url": "http://www.dmtv.ml/mao/live/m3u.txt"},
 {"name": "吾爱",     "url": "http://52bsj.vip:81/api/v3/file/get/763/live.txt?sign=87BTGT1_6AOry7FPwy_uuxFTv2Wcb9aDMj46rDdRTD8%3D%3A0"},
