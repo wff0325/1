217cddde16f126d66529613ac202539bce10ab1c
guot54 <guot55@163.com>
2023-03-16 09:01:17 +0000

    更新 'zB/直播2.js'

--- zB/直播2.js
+++ zB/直播2.js
@@ -1,9 +1,10 @@
 [
+{"name": "🔞",     "url": "https://raw.githubusercontent.com/YanG-1989/m3u/main/Adult.m3u"},
+{"name": "肥羊",     "url": "https://ghproxy.com/https://raw.githubusercontent.com/youshandefeiyang/IPTV/main/main/cqyx.m3u"},
 {"name": "甜蜜",     "url": "http://zdir.kebedd69.repl.co/public/live.txt"},
 {"name": "巧计",     "url": "https://ghproxy.com/https://raw.githubusercontent.com/dxawi/1/main/tvlive.txt"},
 {"name": "星辰",     "url": "http://tvmvip.com/xclive.txt"},
 {"name": "俊于",     "url": "http://home.jundie.top:81/Cat/tv/live.txt"},
-{"name": "多多",     "url": "https://yydsys.top/duo/txt/v.txt"},
 {"name": "菜妮丝",     "url": "http://xn--ihqu10cn4c.xn--z7x900a.love:63/TV/tvzb.txt"},
 {"name": "乱世",     "url": "http://www.dmtv.ml/mao/live/m3u.txt"},
 {"name": "吾爱",     "url": "http://52bsj.vip:81/api/v3/file/get/763/live.txt?sign=87BTGT1_6AOry7FPwy_uuxFTv2Wcb9aDMj46rDdRTD8%3D%3A0"},
