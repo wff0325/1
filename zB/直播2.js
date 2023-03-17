7a2efa248d49062ce27fd5abd7c6ed9dd91cceb4
guot54 <guot55@163.com>
2023-03-17 02:41:34 +0000

    更新 'zB/直播2.js'

--- zB/直播2.js
+++ zB/直播2.js
@@ -1,13 +1,13 @@
 [
-{"name": "🔞",     "url": "https://raw.githubusercontent.com/YanG-1989/m3u/main/Adult.m3u"},
-{"name": "肥羊",     "url": "https://ghproxy.com/https://raw.githubusercontent.com/youshandefeiyang/IPTV/main/main/cqyx.m3u"},
-{"name": "甜蜜",     "url": "http://zdir.kebedd69.repl.co/public/live.txt"},
-{"name": "巧计",     "url": "https://ghproxy.com/https://raw.githubusercontent.com/dxawi/1/main/tvlive.txt"},
-{"name": "星辰",     "url": "http://tvmvip.com/xclive.txt"},
-{"name": "俊于",     "url": "http://home.jundie.top:81/Cat/tv/live.txt"},
-{"name": "菜妮丝",     "url": "http://xn--ihqu10cn4c.xn--z7x900a.love:63/TV/tvzb.txt"},
-{"name": "乱世",     "url": "http://www.dmtv.ml/mao/live/m3u.txt"},
-{"name": "吾爱",     "url": "http://52bsj.vip:81/api/v3/file/get/763/live.txt?sign=87BTGT1_6AOry7FPwy_uuxFTv2Wcb9aDMj46rDdRTD8%3D%3A0"},
-{"name": "月光宝盒ZLIVE",     "url": "https://gitcode.net/ygbh66/dr_py/-/raw/master/zlive.txt"},
-{"name": "饭太硬",     "url": "http://ftyyy.tk/live.txt"}
+{"name": "🔞",     "url": "https://raw.githubusercontent.com/YanG-1989/m3u/main/Adult.m3u&&&https://agit.ai/guot54/ygbh/raw/branch/master/yg.jpg"},
+{"name": "肥羊",     "url": "https://ghproxy.com/https://raw.githubusercontent.com/youshandefeiyang/IPTV/main/main/cqyx.m3u&&&https://agit.ai/guot54/ygbh/raw/branch/master/yg.jpg"},
+{"name": "甜蜜",     "url": "http://zdir.kebedd69.repl.co/public/live.txt&&&https://agit.ai/guot54/ygbh/raw/branch/master/yg.jpg"},
+{"name": "巧计",     "url": "https://ghproxy.com/https://raw.githubusercontent.com/dxawi/1/main/tvlive.txt&&&https://agit.ai/guot54/ygbh/raw/branch/master/yg.jpg"},
+{"name": "星辰",     "url": "http://tvmvip.com/xclive.txt&&&https://agit.ai/guot54/ygbh/raw/branch/master/yg.jpg"},
+{"name": "俊于",     "url": "http://home.jundie.top:81/Cat/tv/live.txt&&&https://agit.ai/guot54/ygbh/raw/branch/master/yg.jpg"},
+{"name": "菜妮丝",     "url": "http://xn--ihqu10cn4c.xn--z7x900a.love:63/TV/tvzb.txt&&&https://agit.ai/guot54/ygbh/raw/branch/master/yg.jpg"},
+{"name": "乱世",     "url": "http://www.dmtv.ml/mao/live/m3u.txt&&&https://agit.ai/guot54/ygbh/raw/branch/master/yg.jpg"},
+{"name": "吾爱",     "url": "http://52bsj.vip:81/api/v3/file/get/763/live.txt?sign=87BTGT1_6AOry7FPwy_uuxFTv2Wcb9aDMj46rDdRTD8%3D%3A0&&&https://agit.ai/guot54/ygbh/raw/branch/master/yg.jpg"},
+{"name": "月光宝盒ZLIVE",     "url": "https://gitcode.net/ygbh66/dr_py/-/raw/master/zlive.txt&&&https://agit.ai/guot54/ygbh/raw/branch/master/yg.jpg"},
+{"name": "饭太硬",     "url": "http://ftyyy.tk/live.txt&&&https://agit.ai/guot54/ygbh/raw/branch/master/yg.jpg"}
 ]
