4fc039b937976aacb84fe955884111b5f57be9b9
guot54 <guot55@163.com>
2023-03-27 03:26:57 +0000

    更新 'zB/直播.js'

--- zB/直播.js
+++ zB/直播.js
@@ -1,6 +1,7 @@
 [
  {"name": "月光宝盒",     "url": "https://agit.ai/guot54/ygbh/raw/branch/master/zB/tvlive.txt&&&https://agit.ai/guot54/ygbh/raw/branch/master/yg.jpg"},
- {"name": "肥羊",     "url": "https://telegram-feiyangdigital.v1.mk/bestv.m3u&&&https://agit.ai/guot54/ygbh/raw/branch/master/yg.jpg"},
+ {"name": "肥羊BESTV",     "url": "https://telegram-feiyangdigital.v1.mk/bestv.m3u&&&https://agit.ai/guot54/ygbh/raw/branch/master/yg.jpg"},
+ {"name": "肥羊重庆有线",     "url": "https://ghproxy.com/https://raw.githubusercontent.com/youshandefeiyang/IPTV/main/main/cqyx.m3u&&&https://agit.ai/guot54/ygbh/raw/branch/master/yg.jpg"},
 {"name": "甜蜜",     "url": "http://zdir.kebedd69.repl.co/public/live.txt&&&https://agit.ai/guot54/ygbh/raw/branch/master/yg.jpg"},
 {"name": "俊于",     "url": "http://home.jundie.top:81/Cat/tv/live.txt&&&https://agit.ai/guot54/ygbh/raw/branch/master/yg.jpg"},
 {"name": "菜妮丝",     "url": "http://xn--ihqu10cn4c.xn--z7x900a.love:63/TV/tvzb.txt&&&https://agit.ai/guot54/ygbh/raw/branch/master/yg.jpg"},
