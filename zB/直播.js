46dfbe817d296023b1c6016086d9c1698774ae75
guot54 <guot55@163.com>
2023-08-08 05:15:59 +0000

    更新 'zB/直播.js'

--- zB/直播.js
+++ zB/直播.js
@@ -1,6 +1,6 @@
 [
  {"name": "MV合集",     "url": "https://agit.ai/guot54/ygbh/raw/branch/master/zB/mv.txt&&&https://agit.ai/guot54/ygbh/raw/branch/master/yg.jpg"},
- {"name": "月光宝盒",     "url": "https://agit.ai/guot54/ygbh/raw/branch/master/zB/tvlive.txt&&&https://agit.ai/guot54/ygbh/raw/branch/master/yg.jpg"},
+ {"name": "MTV合集",     "url": "https://qu.ax/KmzB.txt&&&https://agit.ai/guot54/ygbh/raw/branch/master/yg.jpg"},
 {"name": "甜蜜",     "url": "http://zdir.kebedd69.repl.co/public/live.txt&&&https://agit.ai/guot54/ygbh/raw/branch/master/yg.jpg"},
 {"name": "俊于",     "url": "http://home.jundie.top:81/Cat/tv/live.txt&&&https://agit.ai/guot54/ygbh/raw/branch/master/yg.jpg"},
 {"name": "菜妮丝",     "url": "http://xn--ihqu10cn4c.xn--z7x900a.love:63/TV/tvzb.txt&&&https://agit.ai/guot54/ygbh/raw/branch/master/yg.jpg"},
